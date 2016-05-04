effc95c7f044e8a8524ed206b6c490bcd5b8b67a
Taylor Arellano <tarellano95@yahoo.com>
2016-05-03 17:12:25 -0700

    menu

---
+++ app/src/main/java/lrosenberg/flashcards/ThirdFragment.java
@@ -0,0 +1,21 @@
+package lrosenberg.flashcards;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Taylor Arellano on 4/19/2016.
+ */
+public class ThirdFragment extends Fragment {
+    View myView;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.third_layout, container, false);
+        return myView;
+    }
+}
